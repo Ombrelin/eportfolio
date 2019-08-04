f748588849cf588c13dce583f9f2d85867c16565
Arsène Lapostolet <arsene@lapostolet.fr>
2019-08-04 16:27:06 +0200

    correct ability removal

--- src/main/java/com/arsene/eportfolio/webservices/TechnologyController.java
+++ src/main/java/com/arsene/eportfolio/webservices/TechnologyController.java
@@ -1,7 +1,9 @@
 package com.arsene.eportfolio.webservices;
 
 import com.arsene.eportfolio.exceptions.ResourceNotFoundException;
+import com.arsene.eportfolio.model.data.AbilityRepository;
 import com.arsene.eportfolio.model.data.TechnologyRepository;
+import com.arsene.eportfolio.model.entities.Ability;
 import com.arsene.eportfolio.model.entities.Technology;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,16 +19,18 @@ public class TechnologyController {
     @Autowired
     TechnologyRepository technologyRepository;
 
+    @Autowired
+    AbilityRepository abilityRepository;
 
     @GetMapping("/")
-    public Iterable<Technology> findAll(){
+    public Iterable<Technology> findAll() {
         return technologyRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Technology findById(@PathVariable("id") Integer id){
+    public Technology findById(@PathVariable("id") Integer id) {
         Optional<Technology> t = technologyRepository.findById(id);
-        if(!t.isPresent()){
+        if (!t.isPresent()) {
             throw new ResourceNotFoundException();
         }
         return t.get();
@@ -34,23 +38,34 @@ public class TechnologyController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void delete(@PathVariable("id") Integer id){
-        technologyRepository.deleteById(id);
+    public void delete(@PathVariable("id") Integer id) {
+        Optional<Technology> opt = technologyRepository.findById(id);
+        if (!opt.isPresent()) {
+            throw new ResourceNotFoundException();
+        }
+        Technology t = opt.get();
+
+        for (Ability a : abilityRepository.findAll()) {
+            if (a.getTechnologies().contains(t)) {
+                a.getTechnologies().remove(t);
+                abilityRepository.save(a);
+            }
+        }
     }
 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
-    public Technology create(@RequestBody Technology t){
+    public Technology create(@RequestBody Technology t) {
 
         return technologyRepository.save(t);
     }
 
     @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void update(@PathVariable( "id" ) Integer id, @RequestBody Technology resource) {
+    public void update(@PathVariable("id") Integer id, @RequestBody Technology resource) {
         Optional<Technology> t = technologyRepository.findById(id);
         System.out.println("Techno : " + t.get());
-        if(!t.isPresent()){
+        if (!t.isPresent()) {
             throw new ResourceNotFoundException();
         }
         technologyRepository.save(resource);
