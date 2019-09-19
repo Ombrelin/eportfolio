be7a8c268fc14246f9e6c20c4c324d0e4891fddd
Arsène Lapostolet <arsene@lapostolet.fr>
2019-09-19 16:14:37 +0200

    fix problem with crossorigin3

--- src/main/java/com/arsene/eportfolio/EportfolioApplication.java
+++ src/main/java/com/arsene/eportfolio/EportfolioApplication.java
@@ -2,12 +2,19 @@ package com.arsene.eportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class EportfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EportfolioApplication.class, args);
     }
 
+    @GetMapping("")
+    public String welcome(){
+        return "index.html";
+    }
 }
