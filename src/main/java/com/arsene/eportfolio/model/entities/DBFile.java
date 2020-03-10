2739536779fb6d7e65b28f7255ae373be68df54c
Arsène Lapostolet <arsene@lapostolet.fr>
2020-03-10 14:15:44 +0100

    enabling file upload

---
+++ src/main/java/com/arsene/eportfolio/model/entities/DBFile.java
@@ -0,0 +1,27 @@
+package com.arsene.eportfolio.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "files")
+public class DBFile {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+    private String type;
+
+    @Lob
+    private byte[] data;
+
+}
