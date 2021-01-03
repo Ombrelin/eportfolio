459f79946dff732c136d557713666cf9c0a75ab9
Arsène Lapostolet <arsene@lapostolet.fr>
2021-01-03 22:52:52 +0100

    fix integrations tests for Auth + add swagger + bump java version to 14

--- src/test/java/com/arsene/eportfolio/integration/auth/IntegrationAuth.java
+++ src/test/java/com/arsene/eportfolio/integration/auth/IntegrationAuth.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = EportfolioApplication.class)
 @AutoConfigureMockMvc
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@TestPropertySource(properties = {"username=John Shepard", "password=Tali"})
+@TestPropertySource(properties = {"username=John Shepard", "password=$2y$10$zVpNcmtFnya0GIOb6v3dBeadXX.lVnO2enKc4Uh05bz2Cg63/SLnm"})
 public class IntegrationAuth {
     @Autowired
     private WebApplicationContext context;
@@ -53,10 +55,29 @@ public class IntegrationAuth {
                 .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isOk())
                 .andReturn();
-
+        var token = mvcResult.getResponse().getHeader("Authorization");
         assertNotNull(
                 "JWT Token should be present",
                 mvcResult.getResponse().getHeader("Authorization")
+        );
+    }
+
+    @Test
+    void loginIncorrectCredentials_unauthorized() throws Exception {
+        User user = new User();
+        user.setUsername("John Shepard");
+        user.setPassword("Talii");
+
+        MvcResult mvcResult = mvc.perform(post("/login")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        assertEquals(
+                "JWT Token should be present",
+                401,
+                mvcResult.getResponse().getStatus()
         );
     }
 }
